9ea0822a45178d094f2514e4f193bedfb6884afc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-23 08:53:59 -0700

    weird port

--- server.py
+++ server.py
@@ -31,4 +31,4 @@ def predict():
     return jsonify({'predictions': predictions.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=13311)
